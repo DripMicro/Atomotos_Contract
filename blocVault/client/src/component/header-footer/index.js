9a961f8d19e892bfdf4ccaf61c838940c1020c8d
Drip <whappydrip@gmail.com>
2021-12-13 11:41:58 +0900

    delete Header in index.js

--- blocVault/client/src/component/header-footer/index.js
+++ blocVault/client/src/component/header-footer/index.js
@@ -1,4 +1,4 @@
-export { default as Header } from './Header';
+// export { default as Header } from './Header';
 export { default as Footer } from './Footer';
 export { default as ConnectWallet} from './ConnectWallet';
 export { default as Header_P_T} from './Header_P_T';
